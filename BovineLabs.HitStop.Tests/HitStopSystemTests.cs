0096f61ddb29f027c82d635ff33bb74867d0e357
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-05-06 10:22:50 +0600

    CleanUps + Update Order

--- BovineLabs.HitStop.Tests/HitStopSystemTests.cs
+++ BovineLabs.HitStop.Tests/HitStopSystemTests.cs
@@ -1,7 +1,7 @@
-using BovineLabs.HitStop;
 using BovineLabs.HitStop.Data;
 using BovineLabs.Testing;
 using NUnit.Framework;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -13,120 +13,115 @@ namespace BovineLabs.HitStop.Tests
         [Test]
         public void OnCreate_DoesNotThrow()
         {
-            Assert.DoesNotThrow(() =>
-            {
-                this.World.CreateSystem<HitStopSystem>();
-            });
+            Assert.DoesNotThrow(() => { World.CreateSystem<HitStopSystem>(); });
         }
 
         [Test]
         public void Update_WithNoEntities_DoesNotThrow()
         {
-            var system = this.World.CreateSystem<HitStopSystem>();
-            Assert.DoesNotThrow(() => system.Update(this.WorldUnmanaged));
+            var system = World.CreateSystem<HitStopSystem>();
+            Assert.DoesNotThrow(() => system.Update(WorldUnmanaged));
         }
 
         [Test]
         public void HitStopState_Default_IsEnabledOnEntity()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
-            var entity = this.Manager.CreateEntity(archetype);
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
+            var entity = Manager.CreateEntity(archetype);
 
-            Assert.IsTrue(this.Manager.IsComponentEnabled<HitStopState>(entity));
+            Assert.IsTrue(Manager.IsComponentEnabled<HitStopState>(entity));
         }
 
         [Test]
         public void HitStopState_CanBeEnabled()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
-            var entity = this.Manager.CreateEntity(archetype);
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
+            var entity = Manager.CreateEntity(archetype);
 
-            this.Manager.SetComponentEnabled<HitStopState>(entity, true);
-            Assert.IsTrue(this.Manager.IsComponentEnabled<HitStopState>(entity));
+            Manager.SetComponentEnabled<HitStopState>(entity, true);
+            Assert.IsTrue(Manager.IsComponentEnabled<HitStopState>(entity));
         }
 
         [Test]
         public void HitStopState_CanBeToggled()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
-            var entity = this.Manager.CreateEntity(archetype);
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
+            var entity = Manager.CreateEntity(archetype);
 
-            this.Manager.SetComponentEnabled<HitStopState>(entity, true);
-            Assert.IsTrue(this.Manager.IsComponentEnabled<HitStopState>(entity));
+            Manager.SetComponentEnabled<HitStopState>(entity, true);
+            Assert.IsTrue(Manager.IsComponentEnabled<HitStopState>(entity));
 
-            this.Manager.SetComponentEnabled<HitStopState>(entity, false);
-            Assert.IsFalse(this.Manager.IsComponentEnabled<HitStopState>(entity));
+            Manager.SetComponentEnabled<HitStopState>(entity, false);
+            Assert.IsFalse(Manager.IsComponentEnabled<HitStopState>(entity));
         }
 
         [Test]
         public void HitStopConfig_IsAddedAsComponent()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopConfig));
-            var entity = this.Manager.CreateEntity(archetype);
+            var archetype = Manager.CreateArchetype(typeof(HitStopConfig));
+            var entity = Manager.CreateEntity(archetype);
 
-            Assert.IsTrue(this.Manager.HasComponent<HitStopConfig>(entity));
+            Assert.IsTrue(Manager.HasComponent<HitStopConfig>(entity));
         }
 
         [Test]
         public void HitStopState_RemainingTime_SetAndGet()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
-            var entity = this.Manager.CreateEntity(archetype);
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
+            var entity = Manager.CreateEntity(archetype);
 
-            this.Manager.SetComponentData(entity, new HitStopState { RemainingTime = 0.5f });
-            Assert.AreEqual(0.5f, this.Manager.GetComponentData<HitStopState>(entity).RemainingTime);
+            Manager.SetComponentData(entity, new HitStopState { RemainingTime = 0.5f });
+            Assert.AreEqual(0.5f, Manager.GetComponentData<HitStopState>(entity).RemainingTime);
         }
 
         [Test]
         public void HitStopState_Intensity_SetAndGet()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
-            var entity = this.Manager.CreateEntity(archetype);
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
+            var entity = Manager.CreateEntity(archetype);
 
-            this.Manager.SetComponentData(entity, new HitStopState { CurrentIntensity = 0.3f });
-            Assert.AreEqual(0.3f, this.Manager.GetComponentData<HitStopState>(entity).CurrentIntensity);
+            Manager.SetComponentData(entity, new HitStopState { CurrentIntensity = 0.3f });
+            Assert.AreEqual(0.3f, Manager.GetComponentData<HitStopState>(entity).CurrentIntensity);
         }
 
         [Test]
         public void PostTransformMatrix_WithHitStopState_CanCoexist()
         {
-            var archetype = this.Manager.CreateArchetype(
+            var archetype = Manager.CreateArchetype(
                 typeof(HitStopState),
                 typeof(PostTransformMatrix));
-            var entity = this.Manager.CreateEntity(archetype);
+            var entity = Manager.CreateEntity(archetype);
 
-            this.Manager.SetComponentData(entity, new PostTransformMatrix { Value = float4x4.identity });
-            Assert.IsTrue(this.Manager.HasComponent<PostTransformMatrix>(entity));
+            Manager.SetComponentData(entity, new PostTransformMatrix { Value = float4x4.identity });
+            Assert.IsTrue(Manager.HasComponent<PostTransformMatrix>(entity));
         }
 
         [Test]
         public void MultipleEntities_WithHitStopState_AllEnabledByDefault()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
 
-            using var entities = this.Manager.CreateEntity(archetype, 5, Unity.Collections.Allocator.Temp);
+            using var entities = Manager.CreateEntity(archetype, 5, Allocator.Temp);
 
-            for (int i = 0; i < entities.Length; i++)
-            {
-                Assert.IsTrue(this.Manager.IsComponentEnabled<HitStopState>(entities[i]),
+            for (var i = 0; i < entities.Length; i++)
+                Assert.IsTrue(Manager.IsComponentEnabled<HitStopState>(entities[i]),
                     $"Entity {i} should have HitStopState enabled by default");
-            }
         }
 
         [Test]
         public void MultipleEntities_IndependentState()
         {
-            var archetype = this.Manager.CreateArchetype(typeof(HitStopState));
+            var archetype = Manager.CreateArchetype(typeof(HitStopState));
 
-            using var entities = this.Manager.CreateEntity(archetype, 3, Unity.Collections.Allocator.Temp);
+            using var entities = Manager.CreateEntity(archetype, 3, Allocator.Temp);
 
-            this.Manager.SetComponentData(entities[0], new HitStopState { RemainingTime = 0.1f });
-            this.Manager.SetComponentData(entities[1], new HitStopState { RemainingTime = 0.2f });
-            this.Manager.SetComponentData(entities[2], new HitStopState { RemainingTime = 0.3f });
+            Manager.SetComponentData(entities[0], new HitStopState { RemainingTime = 0.1f });
+            Manager.SetComponentData(entities[1], new HitStopState { RemainingTime = 0.2f });
+            Manager.SetComponentData(entities[2], new HitStopState { RemainingTime = 0.3f });
 
-            Assert.AreEqual(0.1f, this.Manager.GetComponentData<HitStopState>(entities[0]).RemainingTime);
-            Assert.AreEqual(0.2f, this.Manager.GetComponentData<HitStopState>(entities[1]).RemainingTime);
-            Assert.AreEqual(0.3f, this.Manager.GetComponentData<HitStopState>(entities[2]).RemainingTime);
+            Assert.AreEqual(0.1f, Manager.GetComponentData<HitStopState>(entities[0]).RemainingTime);
+            Assert.AreEqual(0.2f, Manager.GetComponentData<HitStopState>(entities[1]).RemainingTime);
+            Assert.AreEqual(0.3f, Manager.GetComponentData<HitStopState>(entities[2]).RemainingTime);
         }
     }
 }
